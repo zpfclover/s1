4e517163066b9dc0f681068841af4e7958d73e23
zpfclover <zpf502@126.com>
2014-09-17 17:21:34 +0800

    9-17

--- do.js
+++ do.js
@@ -1,24 +1,25 @@
-$.get('http://bbs.saraba1st.com/2b/forum-75-2.html',function(data){
+$.get('http://bbs.saraba1st.com/2b/forum-75-1.html',function(data){
 	//$('head').html('');
-	//$('body').html('');
-	$(data).find('#threadlist').find('tbody').each(function(){
+	$('body').html('');
+	$($(data).find('#threadlist')[0]).find('tbody').each(function(){
 		if($(this).attr('id')!=undefined){
 			var id=$(this).attr('id');
 			if(id.indexOf("normalthread") >= 0){
 			
-				var mainA=$(data).find(".common").children('.s');
+				var mainA=$(this).find(".common").children('.s');
 				var title=$(mainA).html();
 				var url=$(mainA).attr('href');
+				console.log(url);
 				$.get(url,function(data_thread){
-					console.log(data_thread);
-					$('body').append(FormThreadBlock(data_thread));
+					//console.log(data_thread);
+					$('body').append(FormThreadBlock(data_thread,url));
 					$('.thread_M').each(function(){
 						var t=$(this).find('.content')[0];
-						console.log($(t)[0].scrollHeight,parseInt($(t).css('max-height')));
-						//if($(t)[0].scrollHeight<=parseInt($(t).css('max-height'))){
-							//console.log('let');
-							//$(this).find('.viewAll')[0].remove();
-						//}
+						//console.log($(t)[0].scrollHeight,parseInt($(t).css('max-height')));
+						if($(t)[0].scrollHeight<=parseInt($(t).css('max-height'))){
+							console.log('let');
+							$(this).find('.viewAll')[0].remove();
+						}
 					});
 				});
 				
@@ -28,7 +29,7 @@ $.get('http://bbs.saraba1st.com/2b/forum-75-2.html',function(data){
 	});
 });
 
-function FormThreadBlock(data){
+function FormThreadBlock(data,url){
 	var mainA=$(data).find("#postlist").children('table')[0];
 	var title=$(data).find('#thread_subject').html();
 	var tag=$(data).find("h1").children('a').html();
@@ -39,7 +40,7 @@ function FormThreadBlock(data){
 	var postTime=L1Obj.posttime;
 	var content=L1Obj.content;
 
-	var html='<div class="thread_M">';
+	var html='<div class="thread_M" url="'+url+'">';
 		html+='<div class="Block">';
 		if(tag!=undefined&&tag!='')
 			html+='<div class="tag">'+tag+'</div>';
@@ -57,27 +58,36 @@ function FormThreadBlock(data){
 			html+='<div class="repo">';
 				for(var i=1;i<postDIVs.length-1;i++){
 					var obj=TrimPostDiv(postDIVs[i]);
-					
-					html+='<div class="re">';
-						html+='<div class="uBlock">';
-							html+='<img src="'+obj.image+'" />';
-							html+='<div class="name">'+obj.username+'</div>';
-							html+='<div class="time">'+obj.posttime+'</div>';
-						html+='</div>';
-						html+='<div class="floor">'+(i+1)+'L</div>';
-						html+='<div class="clear"></div>';
-						html+='<div class="content">'+obj.content+'</div>';
-					html+='</div>';
+					obj.floor=i;
+					html+=FormRepoHtml(obj);
 				}
 			html+='</div>';
 			html+='</div>';
+			html+='<div class="delete">X</div>';
 		html+='</div>';
 		
 	return html;
 }
 
+function FormRepoHtml(obj){
+	var html='';
+	html+='<div class="re" post_id="'+obj.id+'">';
+		html+='<div class="uBlock">';
+			html+='<img src="'+obj.image+'" />';
+			html+='<div class="name">'+obj.username+'</div>';
+			html+='<div class="time">'+obj.posttime+'</div>';
+		html+='</div>';
+		html+='<div class="floor">'+obj.floor+'L</div>';
+		html+='<div class="clear"></div>';
+		html+='<div class="content">'+obj.content+'</div>';
+	html+='</div>';
+	
+	return html
+}
 function TrimPostDiv(data){
 	var obj={};
+	var regex= /\d+/;
+	obj.id=regex.exec($(data).attr('id'));
 	var auth=$(data).find('.pls').find('.authi');
 	obj.username=$(auth).children('a').html();
 	obj.image=$(data).find('.avtm').find('img').attr('src');
@@ -93,20 +103,51 @@ function TrimPostDiv(data){
 	return obj;
 }
 
-			//$(".viewAll").live('click',function(){
-			function OpenOrClose(_this){
-			console.log("sss");
-				if($(_this).attr('O')=='false'){
-					var A=$(_this).prev().children('.content');
-					$(A).addClass('noMaxHeight');
-					$(_this).html('收起');
-					$(_this).attr({'O':'true'});
+AddToHead('jquery.js');
+AddToHead('func.js');
+function AddToHead(title){
+	var oHead = document.getElementsByTagName('HEAD').item(0); 
+    var oScript= document.createElement("script"); 
+    oScript.type = "text/javascript"; 
+    oScript.src=chrome.extension.getURL(title); 
+    oHead.appendChild( oScript); 
+}
+
+setTimeout('IntetvalFunc()',30000);
+function IntetvalFunc(){
+	console.log('auto update repo');
+	$('.thread_M').each(function(){
+		var _this=this;
+		var url=$(_this).attr('url');
+		$.get(url,function(data_thread){
+			var postDIVs=$(data_thread).find("#postlist").children('div');
+			for(var i=1;i<postDIVs.length-1;i++){
+				var obj=TrimPostDiv(postDIVs[i]);
+				var lastID=$(_this).find('.re');
+				var newReCount=0;
+				var info=$(_this).find('.info');
+					if(info.length>0){
+						var regex= /\d+/;
+						newReCount=regex.exec($(info).html())
+					}
+				var lastFloor=parseInt($(lastID[lastID.length-1]).find('.floor').html());
+				lastID=$(lastID[lastID.length-1]).attr('post_id');
+				if(obj.id>lastID){
+					lastFloor++;
+					obj.floor=lastFloor;
+					$(_this).find('.repo').append(FormRepoHtml(obj));
+					newReCount++;
 				}
-				else{
-					var A=$(_this).prev().children('.content');
-					$(A).removeClass('noMaxHeight');
-					$(_this).html('阅读完整内容');
-					$(_this).attr({'O':'false'});
+				if(newReCount>0){
+					var info=$(_this).find('.info');
+					if(info.length==0){
+						$(_this).prepend('<div class="info"></div>');
+						info=$(_this).find('.info');
+					}
+					$(info).html('有'+newReCount+'条新回复');
 				}
 			}
-			//});
+		});
+	});
+	setTimeout('IntetvalFunc()',30000);
+}
---
+++ func.js
@@ -0,0 +1,23 @@
+﻿		function OpenOrClose(_this){
+				if($(_this).attr('O')=='false'){
+					var A=$(_this).prev().children('.content');
+					$(A).addClass('noMaxHeight');
+					$(_this).html('收起');
+					$(_this).attr({'O':'true'});
+				}
+				else{
+					var A=$(_this).prev().children('.content');
+					$(A).removeClass('noMaxHeight');
+					$(_this).html('阅读完整内容');
+					$(_this).attr({'O':'false'});
+				}
+			}
+		$(document).ready(function(){
+			$('.delete').on('click',function(){
+				$(this).parent().remove();
+			});
+			
+			$('.repo').on('scroll',function(){
+				$(this).parent().parent().find('.info').remove();
+			});
+		});
